4489721e258d9acb766c4b9e97b3f1456ab9dabc
Khaled El-Tahan <millerzbusiness@gmail.com>
2020-09-29 14:42:02 +0200

    Add dataset utilities

---
+++ src/datasets/transform_helper.py
@@ -0,0 +1,10 @@
+"""Apply set of transformations using the configurations object"""
+import src.datasets.transform as transform
+import src.datasets.transform_cv2 as transform_cv2
+
+
+def apply_transformations(data, cfg):
+    """
+        
+    """
+    pass
