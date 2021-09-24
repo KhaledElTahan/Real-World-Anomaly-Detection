db25c1186393cd90f339e38e393d96cdca6da9d2
KhaledElTahan <khaled.m.eltahan@gmail.com>
2021-09-24 02:50:21 +0200

    Add notes on how to activate untracked configs

--- src/config/custom_untracked_config.py
+++ src/config/custom_untracked_config.py
@@ -4,6 +4,11 @@
 def add_custom_untracked_config(cfg):
     """
     First Highest priority configurations, untracked on git
+    NOTE: You must run the following git command in the project directory to be effective
+        git update-index --assume-unchanged src/config/custom_untracked_config.py
+    NOTE: To undo the above command and make changes noticeable by git
+        (to make changes for the config functionalty and not the actual configs)
+        git update-index --no-assume-unchanged src/config/custom_untracked_config.py
     Args
         cfg (cfgNode): Model configurations
     """
