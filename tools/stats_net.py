38ec6ed244d27eb49f3603ecad7a0f369883050b
KhaledElTahan <khaled.m.eltahan@gmail.com>
2021-09-25 20:50:45 +0200

    Beautify stats net output

--- tools/stats_net.py
+++ tools/stats_net.py
@@ -46,24 +46,30 @@ def _print_trained_models_stats(cfg):
                     infoutils.get_full_model_without_features(checkpoint_cfg),
                     checkpoint_cfg.TRAIN.DATA_READ_ORDER,
                     "{:6f}".format(best_auc),
-                    best_epoch,
+                    str(best_epoch),
                     "{:6f}".format(auc),
-                    epoch
+                    str(epoch)
                 ]
             )
 
     table.sort(key=lambda row: row[1])
     table.sort(key=lambda row: row[0])
 
+    table.append([''] * len(headers))
     spaced_table = []
+    rows = []
     for index, row in enumerate(table):
         if index > 0 and table[index][0] != table[index - 1][0]:
-            spaced_table.append(['---'] * len(headers))
-        spaced_table.append(row)
+            spaced_table.append(rows)
+            rows = []
+
+        if len(rows) == 0:
+            rows = row
+        else:
+            for idx, col in enumerate(row):
+                rows[idx] += '\n' + col
 
     print(tabulate(
-        spaced_table, headers, tablefmt="pretty",
+        spaced_table, headers, tablefmt="fancy_grid",
         colalign=("center", "center", "center", "center", "center", "center", "center"))
     )
-
-    print()
