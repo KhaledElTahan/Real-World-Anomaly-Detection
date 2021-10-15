ceae330b67e29480622dc8096959f0607711e37c
KhaledElTahan <khaled.m.eltahan@gmail.com>
2021-10-15 21:31:37 +0200

    Add features download links to script

--- scripts/download_features.sh
+++ scripts/download_features.sh
@@ -16,7 +16,20 @@ mkdir features
 cd features
 
 # SlowFast Features
+wget -O Kinetics_c2_SLOWFAST_8x8_R50_NONE_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140087&authkey=AMirV3Fulu9UKj4"
 wget -O Kinetics_c2_SLOWFAST_8x8_R50_BG-KNN_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140076&authkey=ABmXmJlleHD0og0"
+wget -O Kinetics_c2_SLOWFAST_8x8_R50_BG-MOG2_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140086&authkey=AE139_PjlPADqfA"
+
+# MViT K400 Features
+wget -O Kinetics_MVIT_B_32x3_CONV_K400_NONE_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140127&authkey=ANvvAcit-mr8qaE"
+wget -O Kinetics_MVIT_B_32x3_CONV_K400_BG-KNN_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140128&authkey=APYAetItQuvgGtY"
+wget -O Kinetics_MVIT_B_32x3_CONV_K400_BG-MOG2_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140129&authkey=APDONysZE0xLm9g"
+
+# MViT K600 Features
+wget -O Kinetics_MVIT_B_32x3_CONV_K600_NONE_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140124&authkey=ACxVV5fIAZCEzqk"
+wget -O Kinetics_MVIT_B_32x3_CONV_K600_BG-KNN_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140125&authkey=AP495bZ_OVEEmic"
+wget -O Kinetics_MVIT_B_32x3_CONV_K600_BG-MOG2_32x32.zip "https://onedrive.live.com/download?cid=43A1E31F48BBE205&resid=43A1E31F48BBE205%2140126&authkey=AHcixzMloYdjqLs"
+
 
 # MViT Features
 
